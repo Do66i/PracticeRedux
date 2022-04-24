97154393ccf5520a54a14211e58fc29298effe51
DOBBI <cocomong_94@naver.com>
2022-04-24 23:45:10 +0900

    REDUX 폐관수련

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,10 +4,35 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const reducer = () => {
-  return "Hello";
+const reducer = (count = 0, action) => {
+  // data의 modifier = reducer
+  if (action.type === "ADD") {
+    // 전송한 msg의 action을 췍
+    console.log("만약 'ADD'가 실행된다면 도토잠보를 보여주세요 🐘");
+    return ++count;
+  } else if (action.type === "MINUS") {
+    console.log("만약 'ADD'가 실행된다면 물잇구럭을 보여주세요 🦭");
+    return --count;
+  } else {
+    return count;
+  }
 };
 
-const countStore = createStore(reducer);
+const countStore = createStore(reducer); // data의 store을 create하고
 
-console.log(countStore);
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
